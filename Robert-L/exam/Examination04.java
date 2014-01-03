2458764d67ecadf152f44a103f0a3e0f8170a9df
Robert-L <1308731627@qq.com>
2014-01-03 21:42:16 +0800

    2014.1.3

---
+++ Robert-L/exam/Examination04.java
@@ -0,0 +1,20 @@
+package com.eoe.exam;
+
+public class Examination04 {
+	/**
+	 * 编程查找100~300之间所有的质数。
+	 */
+	public static void main(String[] args) {
+		for (int i = 101; i < 300; i += 2) {
+			boolean isPrime = true;
+			int k = (int) Math.sqrt(i);
+			for (int j = 2; j <= k && isPrime; j++) {
+				isPrime = i % j != 0;
+			}
+			if (isPrime) {
+				System.out.println(i);
+			}
+		}
+
+	}
+}
